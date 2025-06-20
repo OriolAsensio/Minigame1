5e9a3b2bb792a69bd4641e89d8c530ad50a2c6ca
OriolAsensio <oasensioserra@gmail.com>
2025-06-20 16:14:23 +0200

    Score, and text
    Missing: Main Menu, other OBSTACLES, the platforms must move faster when the time pases.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject gameOverScreen;
+    [SerializeField] private TMP_Text scoretext;
+
+    private int score;
+    private float timer;
+
+
+
     public static GameManager Instance { get; private set; }
 
 
@@ -26,13 +35,30 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        UpdateScore();
     }
 
     public void ShowGameOverScreen()
     { 
     
     gameOverScreen.SetActive(true);
+    
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f; 
+    }
+
+    private void UpdateScore()
+    {
+
+        int scoreperSecond = 10;
+
+        timer += Time.deltaTime;
+        score = (int)(timer * scoreperSecond);
+        scoretext.text = string.Format("{0:00000}", score);
     
     }
 }
